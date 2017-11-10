edd22916baadc61d39e8840dedec5cb281f610be
Eric Hess <eric-hess@outlook.com>
2017-11-10 15:41:08 +0100

    added a debug presentation option

--- WhatsNew/Items/PresentationOption.swift
+++ WhatsNew/Items/PresentationOption.swift
@@ -9,10 +9,12 @@ import Foundation
 
 /// Defines when the "What's New" view controller is presented.
 public enum PresentationOption {
-    /// Always present if the main bundle version changes.
+    /// present if the main bundle version changes.
     case always
     /// Only present if the main bundle changes major version.
     case majorVersion
     /// Never present.
     case never
+    /// present always
+    case debug
 }
