4b1357112fb2be4e23d9a0c6ddc397e8b09cc7d6
Stefan Trauth <mail@stefantrauth.de>
2018-03-31 14:20:05 +0200

    fix test cases

--- Example/Tests/WhatsNewTests.swift
+++ Example/Tests/WhatsNewTests.swift
@@ -17,37 +17,59 @@ class WhatsNewTests: XCTestCase {
         UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
     }
     
+    func testShouldNotPresent_WhenCurrentVersionIsUndefined() {
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .always, currentVersion: nil))
+    }
+    
+    
+    
+    
+    
     func testShouldPresentAlways_WhenFirstVersionInstalled() {
-        XCTAssertTrue(WhatsNew.shouldPresent(with: .always))
+        XCTAssertTrue(WhatsNew.shouldPresent(with: .always, currentVersion: "1.0"))
     }
 
     func testShouldPresentAlways_WhenUpdatingVersion() {
-        UserDefaults.standard.set("0.9", forKey: WhatsNew.userDefaultsKey)
-        XCTAssertTrue(WhatsNew.shouldPresent(with: .always))
+        UserDefaults.standard.set("1.0", forKey: WhatsNew.userDefaultsKeyLatestAppVersionPresented)
+        XCTAssertTrue(WhatsNew.shouldPresent(with: .always, currentVersion: "1.1"))
+        XCTAssertTrue(WhatsNew.shouldPresent(with: .always, currentVersion: "1.0.1"))
     }
 
-    func testShouldNotPresentAlways_WhenNotUpdatingVersion() {
-        UserDefaults.standard.set("1.0", forKey: WhatsNew.userDefaultsKey)
-        XCTAssertFalse(WhatsNew.shouldPresent(with: .always))
+    func testShouldNotPresent_WhenNotUpdatingVersion() {
+        UserDefaults.standard.set("1.0", forKey: WhatsNew.userDefaultsKeyLatestAppVersionPresented)
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .always, currentVersion: "1.0"))
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .always, currentVersion: "1.0.0"))
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .always, currentVersion: "1"))
     }
 
+    
+    
+    
     func testShouldPresentMajorVersion_WhenUpdatingVersionMajor() {
-        UserDefaults.standard.set("0.9", forKey: WhatsNew.userDefaultsKey)
-        XCTAssertTrue(WhatsNew.shouldPresent(with: .majorVersion))
+        UserDefaults.standard.set("1.9", forKey: WhatsNew.userDefaultsKeyLatestAppVersionPresented)
+        XCTAssertTrue(WhatsNew.shouldPresent(with: .majorVersion, currentVersion: "2.0"))
+        XCTAssertTrue(WhatsNew.shouldPresent(with: .majorVersion, currentVersion: "2.0.0"))
     }
 
     func testShouldNotPresentMajorVersion_WhenUpdatingVersionMinor() {
-        UserDefaults.standard.set("1.0.1", forKey: WhatsNew.userDefaultsKey)
-        XCTAssertFalse(WhatsNew.shouldPresent(with: .majorVersion))
-    }
-
-    func testShouldNotPresentNever_WhenUpdatingVersion() {
-        UserDefaults.standard.set("0.9", forKey: WhatsNew.userDefaultsKey)
-        XCTAssertFalse(WhatsNew.shouldPresent(with: .never))
+        UserDefaults.standard.set("1.0", forKey: WhatsNew.userDefaultsKeyLatestAppVersionPresented)
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .majorVersion, currentVersion: "1.1"))
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .majorVersion, currentVersion: "1.0.1"))
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .majorVersion, currentVersion: "2.1"))
     }
     
+    
+    
+
+    func testShouldNotPresentNever_WhenUpdatingVersion() {
+        UserDefaults.standard.set("1.0", forKey: WhatsNew.userDefaultsKeyLatestAppVersionPresented)
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .never, currentVersion: "1.0"))
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .never, currentVersion: nil))
+        XCTAssertFalse(WhatsNew.shouldPresent(with: .never, currentVersion: "2.0"))
+    }
+    
+    
     func testShouldPresent_WhenDebugPresentationOption() {
-        UserDefaults.standard.set("1.0", forKey: WhatsNew.userDefaultsKey)
-        XCTAssertTrue(WhatsNew.shouldPresent(with: .debug))
+        XCTAssertTrue(WhatsNew.shouldPresent(with: .debug, currentVersion: "2.4"))
     }
 }
